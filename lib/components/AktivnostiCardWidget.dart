e2aeca58dfe5f9a03320d940e5d3136802e4cbd8
Milorad <nedovicmilorad15@gmail.com>
2023-12-10 16:04:07 +0100

    added layout for Aktivnosti screen

---
+++ lib/components/AktivnostiCardWidget.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
+import 'package:intl/intl.dart';
+
+class AktivnostiCardWidget extends StatefulWidget {
+
+  final String title;
+  final DateTime dateTime;
+  final TimeOfDay time;
+  final String location;
+  final int participants;
+
+  AktivnostiCardWidget({
+    required this.title,
+    required this.dateTime,
+    required this.time,
+    required this.location,
+    required this.participants,
+  });
+
+  @override
+  _AktivnostiCardWidgetState createState() => _AktivnostiCardWidgetState();
+}
+
+class _AktivnostiCardWidgetState extends State<AktivnostiCardWidget> {
+
+  bool isJoining = true;
+
+  @override
+  Widget build(BuildContext context) {
+    final medijakveri = MediaQuery.of(context);
+    final dateFormat = DateFormat('dd/MM/yyyy');
+    return Column(
+      children: [
+        Center(
+          child: Card(
+            elevation: 0.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+              side: BorderSide(color: Theme.of(context).primaryColor)
+            ),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: 0, maxHeight: double.infinity),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.title,
+                          style: Theme.of(context).textTheme.headline2?.copyWith(
+                            color: Theme.of(context).colorScheme.tertiary,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Text(
+                              'Vrijeme',
+                              style: Theme.of(context).textTheme.headline3,
+                            ),
+                            SizedBox(width: 5,),
+                            Icon(TablerIcons.clock),
+                          ],
+                        ),
+                        SizedBox(height: 5,),
+                        Text(
+                          "${dateFormat.format(widget.dateTime)} | ${widget.time.format(context)}",
+                          style: Theme.of(context).textTheme.headline4,
+                        ),
+                        SizedBox(height: 10,),
+                        Row(
+                          children: [
+                            Text(
+                              'Lokacija',
+                              style: Theme.of(context).textTheme.headline3,
+                            ),
+                            SizedBox(width: 5,),
+                            Icon(TablerIcons.map_pin_filled),
+                          ],
+                        ),
+                        SizedBox(height: 5,),
+                        Row(
+                          children: [
+                            Text(
+                              widget.location,
+                              style: Theme.of(context).textTheme.headline4?.copyWith(
+                                color: Theme.of(context).primaryColor,
+                              )
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10,),
+                        Row(
+                          children: [
+                            Text(
+                              "Broj učesnika: ${widget.participants}",
+                              style: Theme.of(context).textTheme.headline3,
+                            ),
+                            SizedBox(width: 5,),
+                            Icon(TablerIcons.users),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10.0),
+                    child: FractionallySizedBox(
+                      widthFactor: 0.6,
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              isJoining = !isJoining;
+                            });
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: isJoining
+                                ? Theme.of(context).colorScheme.primary
+                                : Colors.white,
+                            onPrimary: isJoining ? Colors.white : Colors.black,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(7.0),
+                              side: BorderSide(
+                                color: isJoining
+                                    ? Theme.of(context).colorScheme.primary
+                                    : Theme.of(context).colorScheme.primary,
+                              ),
+                            ),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              isJoining
+                                  ? Icon(TablerIcons.circle_check, size: 24,)
+                                  : Icon(TablerIcons.circle_x, size: 24, color: Theme.of(context).colorScheme.primary,),
+                              SizedBox(width: 10),
+                              Text(
+                                isJoining ? 'Pridruži se' : 'Odustani',
+                                style: TextStyle(
+                                  fontSize: 16.0,
+                                  color: isJoining ? Colors.white : Theme.of(context).colorScheme.primary,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                ],
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: (medijakveri.size.height - medijakveri.padding.top) * 0.015),
+      ],
+    );
+  }
+}
